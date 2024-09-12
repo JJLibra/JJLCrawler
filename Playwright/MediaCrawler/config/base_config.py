cad483959612a005093f3a055bc9af106e9a4469
JJLibra <2565474095@qq.com>
2024-09-12 15:37:43 +0800

    🐎 ci: 删除相关依赖文件

--- Playwright/MediaCrawler/config/base_config.py
+++
@@ -1,5 +0,0 @@
-# 默认配置项
-PLATFORM = 'bili'
-LOGIN_TYPE = 'qrcode'
-CRAWLER_TYPE = 'search'
-START_PAGE = 1
--- Scrapy/pksSpider/main.py
+++
@@ -1,3 +0,0 @@
-from scrapy import cmdline
-
-cmdline.execute('scrapy crawl pks -o test3.json -t json'.split())
