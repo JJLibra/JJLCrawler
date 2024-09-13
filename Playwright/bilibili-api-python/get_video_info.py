28e2de36c4695304509ea7cad1ae6a27ee8c183e
JJLibra <2565474095@qq.com>
2024-09-13 23:53:34 +0800

    ✨ feat: 异步爬取指定视频信息

---
+++ Playwright/bilibili-api-python/get_video_info.py
@@ -0,0 +1,29 @@
+import config
+import asyncio
+
+from typing import Dict
+from bilibili_api import video, Credential
+from handle_file import FileHandler
+
+
+async def get_video_info():
+    """
+    example:
+    Crawl https://www.bilibili.com/video/BV1ej421S7wn/?spm_id_from=333.788&vd_source=91a233ea2e45cea087336c119461d12b video information
+    :return: None
+    """
+    bv_id = 'BV1ej421S7wn'
+
+    credential = Credential(sessdata=config.SESSDATA, bili_jct=config.BILI_JCT, buvid3=config.BUVID3)
+    v = video.Video(bvid=bv_id, credential=credential)
+    info = await v.get_info()
+
+    await file_write_to_json(bv_id=bv_id, data=info)
+
+
+async def file_write_to_json(bv_id: str, data: Dict):
+    file_handler = FileHandler()
+    file_handler.save_json(f'{bv_id}.json', data=data)
+
+
+asyncio.run(get_video_info())
