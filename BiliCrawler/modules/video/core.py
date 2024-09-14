b9522ed4a65f50b57e758039b07ffdbc5c22ec88
JJLibra <2565474095@qq.com>
2024-09-14 22:41:12 +0800

    ✨ feat: 视频信息模块

--- BiliCrawler/modules/video/core.py
+++ BiliCrawler/modules/video/core.py
@@ -1,7 +1,8 @@
-from base.base_crawler import AbstractCrawler
 from PyInquirer import prompt
+
+from modules.video import upload, info, comments, barrages
+from base.base_crawler import AbstractCrawler
 from tools.utils import logger
-import info, upload, comments
 
 
 class VideoCrawler(AbstractCrawler):
@@ -11,12 +12,14 @@ class VideoCrawler(AbstractCrawler):
 
         mode = await self.select_mode()
 
-        if mode == 'download':
+        if mode == 'upload':
             await upload.upload_video()
         elif mode == 'info':
             await info.fetch_video_info()
         elif mode == 'comments':
             await comments.fetch_video_comments()
+        elif mode == 'barrages':
+            await barrages.fetch_video_barrages()
         else:
             logger.error("Invalid mode selected.")
             print("Invalid mode selected. Please choose 'upload', 'info', or 'comments'.")
@@ -33,7 +36,8 @@ class VideoCrawler(AbstractCrawler):
                 'choices': [
                     'Upload video',
                     'Fetch video info',
-                    'Fetch video comments'
+                    'Fetch video comments',
+                    'Fetch video barrages'
                 ]
             }
         ]
@@ -43,9 +47,11 @@ class VideoCrawler(AbstractCrawler):
         logger.info(f"Mode selected: {answers['mode']}")
 
         if answers['mode'] == 'Upload video':
-            return 'download'
+            return 'upload'
         elif answers['mode'] == 'Fetch video info':
             return 'info'
         elif answers['mode'] == 'Fetch video comments':
             return 'comments'
+        elif answers['mode'] == 'Fetch video barrages':
+            return 'barrages'
 
