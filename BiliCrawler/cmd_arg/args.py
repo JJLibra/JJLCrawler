33102b90b5f53fb04f6b826face67ebd2c19a93f
JJLibra <2565474095@qq.com>
2024-09-14 21:04:39 +0800

    🦄 refactor: 无用参数删减

--- BiliCrawler/cmd_arg/args.py
+++ BiliCrawler/cmd_arg/args.py
@@ -9,8 +9,6 @@ async def parse_cmd():
                                                    ' music | emoji | ip_address)',
                         choices=['search', 'user', 'video', 'topic', 'music', 'emoji', 'ip_address'],
                         default=config.MODULE)
-    parser.add_argument('--login', type=str, help='Login type (qrcode | phone | passwd)',
-                        choices=['qrcode', 'phone', 'passwd'], default=config.LOGIN_TYPE)
     parser.add_argument('--start', type=int,
                         help='Number of start page', default=config.START_PAGE)
 
@@ -18,5 +16,4 @@ async def parse_cmd():
 
     # override config
     config.MODULE = args.module
-    config.LOGIN_TYPE = args.login
     config.START_PAGE = args.start
