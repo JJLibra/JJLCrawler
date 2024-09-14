d8e8b1dd3295ac079a693dfa49ef0020689f9fa0
JJLibra <2565474095@qq.com>
2024-09-14 21:04:05 +0800

    ✨ feat: video模块框架

---
+++ BiliCrawler/modules/video/info.py
@@ -0,0 +1,8 @@
+from tools.utils import logger
+
+
+async def fetch_video_info():
+    logger.info("Fetching video info started.")
+
+    logger.info("Fetching video info finished successfully.")
+
---
+++ BiliCrawler/modules/video/upload.py
@@ -0,0 +1,8 @@
+from tools.utils import logger
+
+
+async def upload_video():
+    logger.info("Uploading video started.")
+
+    logger.info("Uploading video finished successfully.")
+
